8654cce3e29cf9f0b4a6f0379d40bd6129e11d09
Rene Pierre Ibrahima GUEYE <rpgueye@ubuntuvdi08.sciences.lan>
2019-10-20 14:20:55 +0200

    Tp4

--- tp4/src/Vol.java
+++ tp4/src/Vol.java
@@ -1,25 +1,48 @@
-//package voyages;
 import java.util.*;
 import java.time.*;
+import java.lang.*;
 
-public class Vol extends Reservation
-{
+
+public class Vol {
+
     private String identifiant;
     private ZonedDateTime depart;
-    private ZonedDateTime arrivee;    
+    private ZonedDateTime arrivee;
+    public final int NBRPLACE = 4;
+    public int compte = 0;
 
-    public void duree()
-    {
-
+    public Vol (String id, ZonedDateTime dep, ZonedDateTime arr) {
+        this.identifiant = id;
+        this.depart = dep;
+        this.arrivee = arr;
+        
     }
 
-    public void ouvrir()
-    {
+    public Duration duree() {
 
+        return(Duration.between(this.arrivee,this.depart));
     }
-    public void fermer()
-    {
 
+    public int ouvrir() {
+        if(compte == NBRPLACE){
+            this.fermee();
+            return -1;
+        }
+        System.out.println("Vol est ouvert");
+        return 0;
+    }
+
+    public int fermee() {
+        if( compte < NBRPLACE) {
+            this.ouvrir();
+            return 0;
+        }
+        System.out.println("Vol est plein");
+        return -1;
+    }
+
+    public String getId(){
+        return this.identifiant;
     }
 
 }
